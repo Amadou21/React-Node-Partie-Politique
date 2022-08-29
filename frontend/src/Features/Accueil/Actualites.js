7c009e854d282edfc44e7d9f491f92ded8570514
Amadou21 <ad.maiga2107@gmail.com>
2022-08-29 13:44:29 +0000

    Mise en place

---
+++ frontend/src/Features/Accueil/Actualites.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppLayout from '../Layout/AppLayout';
+
+const Actualites = () => {
+    return (
+        <AppLayout>
+            <div>
+                Actualite
+            </div>
+        </AppLayout>
+    );
+};
+
+export default Actualites;
---
+++ frontend/src/Features/Membres/MembreRecherche.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppLayout from '../Layout/AppLayout';
+
+const MembreRecherche = () => {
+    return (
+        <AppLayout>
+            <div>
+                <h1>Recherche</h1>
+            </div>
+        </AppLayout>
+    );
+};
+
+export default MembreRecherche;
