55094a4fdbf6ef2d9b108bbd2f85341a73133724
Amadou21 <ad.maiga2107@gmail.com>
2022-08-29 11:26:32 +0000

    misenen place

---
+++ Frontend/src/Features/Accueil/Accueil.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from '../Layout/AppLayout';
+
+
+const Accueil = () => {
+    return (
+        <div>Accueil</div>
+
+    );
+};
+
+export default Accueil;
