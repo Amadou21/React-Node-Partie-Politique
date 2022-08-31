6568d010b958afd1e0742c32f16ccd4765a9d8ef
Aly DIARRA <alydiarra2205@gmail.com>
2022-08-31 10:08:33 +0000

    10h09 push depuis aly2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,52 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Accueil from './Features/Accueil/Accueil'
-import Actualites from './Features/Accueil/Actualites';
-import Projet from './Features/Accueil/Projet';
-import Connexion from './Features/Membres/Connexion';
-import MembreActualite from './Features/Membres/MembreActualite';
-import MembreRecherche from './Features/Membres/MembreRecherche';
-
+import Accueil from "./Features/Accueil/Accueil";
+import Actualites from "./Features/Accueil/Actualites";
+import Projet from "./Features/Accueil/Projet";
+import AppLayout from "./Features/Layout/AppLayout";
+import Connexion from "./Features/Membres/Connexion";
+import MembreActualite from "./Features/Membres/MembreActualite";
+import MembreRecherche from "./Features/Membres/MembreRecherche";
+import TableauDeBord from "./Features/Users/TableauDeBord";
+import UserCompte from "./Features/Users/UserCompte";
+import UserDetail from "./Features/Users/UserDetail";
+import UserProfil from "./Features/Users/UserProfil";
 
 const App = () => {
-
   const routes = [
-    { link: '/', element: <Accueil /> },
-    { link: '/projet', element: <Projet /> },
-    { link: '/actualites', element: <Actualites /> },
-    { link: '/connexion', element: <Connexion /> },
-    { link: '/membre-actualites', element: <MembreActualite /> },
-    { link: '/membre-recherche', element: <MembreRecherche /> },
-  ]
+    { link: "/", element: <Accueil /> },
+    { link: "/projet", element: <Projet /> },
+    { link: "/actualites", element: <Actualites /> },
+    { link: "/connexion", element: <Connexion /> },
+    { link: "/membre-actualites", element: <MembreActualite /> },
+    { link: "/membre-recherche", element: <MembreRecherche /> },
+
+    { link: "/compte/:id", element: <UserCompte /> },
+    { link: "/tableauDeBord", element: <TableauDeBord /> },
+    { link: "/userDetail/:id", element: <UserDetail /> },
+
+    { link: "/userProfil/:id", element: <UserProfil /> },
+  ];
 
   const routesMembre = [
-    { link: '/membre-actualites', element: <MembreActualite /> },
-    { link: '/membre-recherche', element: <MembreRecherche /> },
-  ]
+    { link: "/membre-actualites", element: <MembreActualite /> },
+    { link: "/membre-recherche", element: <MembreRecherche /> },
+  ];
 
   return (
     <BrowserRouter>
-      <Routes>
-        {routes.map(route => (
-          <Route key={route.link} path={route.link} element={route.element} />
-        ))}
+      <AppLayout>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.link} path={route.link} element={route.element} />
+          ))}
 
-        {routesMembre.map(route => (
-          <Route key={route.link} path={route.link} element={route.element} />
-        ))}
-
-      </Routes>
+          {routesMembre.map((route) => (
+            <Route key={route.link} path={route.link} element={route.element} />
+          ))}
+        </Routes>
+      </AppLayout>
     </BrowserRouter>
   );
 };
