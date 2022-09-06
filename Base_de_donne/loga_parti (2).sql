4f8de6a0bcebae1d7acbac0af1c6a3f78e15544e
Amadou21 <ad.maiga2107@gmail.com>
2022-09-06 12:40:23 +0000

    Maj Bd

---
+++ Base_de_donne/loga_parti (2).sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 06 sep. 2022 à 14:39
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `loga_parti`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bureau`
+--
+
+CREATE TABLE `bureau` (
+  `id_bureau` int(11) NOT NULL,
+  `libelle_bureau` varchar(40) NOT NULL,
+  `description` text NOT NULL,
+  `photo_bureau` longblob DEFAULT NULL,
+  `id_localite` int(11) NOT NULL,
+  `id_type` int(11) NOT NULL,
+  `id_mandat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `localite`
+--
+
+CREATE TABLE `localite` (
+  `id_localite` int(11) NOT NULL,
+  `libelle_localite` varchar(40) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `id_region` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mandat`
+--
+
+CREATE TABLE `mandat` (
+  `id_mandat` int(11) NOT NULL,
+  `debut` date NOT NULL,
+  `fin` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+--
+
+CREATE TABLE `membre` (
+  `id_membre` int(11) NOT NULL,
+  `nom` varchar(40) NOT NULL,
+  `prenom` varchar(40) NOT NULL,
+  `photo_membre` mediumblob DEFAULT NULL,
+  `id_bureau` int(11) NOT NULL,
+  `id_poste` int(11) NOT NULL,
+  `libelle_poste` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pays`
+--
+
+CREATE TABLE `pays` (
+  `id_pays` int(11) NOT NULL,
+  `libelle_pays` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `poste`
+--
+
+CREATE TABLE `poste` (
+  `id_poste` int(11) NOT NULL,
+  `libelle_poste` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `poste`
+--
+
+INSERT INTO `poste` (`id_poste`, `libelle_poste`) VALUES
+(1, 'Président'),
+(2, 'Vice-Président');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `region`
+--
+
+CREATE TABLE `region` (
+  `id_region` int(11) NOT NULL,
+  `libelle_region` varchar(40) NOT NULL,
+  `nb_bureau` varchar(45) NOT NULL,
+  `id_pays` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type`
+--
+
+CREATE TABLE `type` (
+  `id_type` int(11) NOT NULL,
+  `libelle_type` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `type`
+--
+
+INSERT INTO `type` (`id_type`, `libelle_type`) VALUES
+(1, 'Bureau national'),
+(2, 'Bureau regonial');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `login` varchar(40) NOT NULL,
+  `mot_de_pass` varchar(40) NOT NULL,
+  `prenom` varchar(45) DEFAULT NULL,
+  `nom` varchar(45) DEFAULT NULL,
+  `photo_user` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `login`, `mot_de_pass`, `prenom`, `nom`, `photo_user`) VALUES
+(1, 'amadou@gmail.com', 'qw', 'Amadou', 'Maiga', NULL),
+(8, 'amadoumaiga1@gmail.com', 'qw', 'Amadou', 'Maiga', NULL),
+(9, 'amadoumaiga2@gmail.com', 'qw', 'Amadou', 'Maiga', NULL),
+(10, 'amadoumaiga3@gmail.com', 'qw', 'Amadou', 'Maiga', NULL),
+(11, 'amadoumaiga4@gmail.com', 'qw', 'Amadou', 'Maiga', NULL),
+(12, 'amadoumaiga5@gmail.com', 'qw', 'Amadou', 'Maiga', NULL),
+(13, 'amadoumaiga6@gmail.com', 'qw', 'Amadou', 'Maiga', NULL);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `bureau`
+--
+ALTER TABLE `bureau`
+  ADD PRIMARY KEY (`id_bureau`),
+  ADD KEY `id_localite` (`id_localite`),
+  ADD KEY `id_type` (`id_type`),
+  ADD KEY `id_mandat` (`id_mandat`);
+
+--
+-- Index pour la table `localite`
+--
+ALTER TABLE `localite`
+  ADD PRIMARY KEY (`id_localite`),
+  ADD KEY `id_region` (`id_region`);
+
+--
+-- Index pour la table `mandat`
+--
+ALTER TABLE `mandat`
+  ADD PRIMARY KEY (`id_mandat`);
+
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`id_membre`),
+  ADD KEY `id_bureau` (`id_bureau`),
+  ADD KEY `id_poste` (`id_poste`);
+
+--
+-- Index pour la table `pays`
+--
+ALTER TABLE `pays`
+  ADD PRIMARY KEY (`id_pays`);
+
+--
+-- Index pour la table `poste`
+--
+ALTER TABLE `poste`
+  ADD PRIMARY KEY (`id_poste`);
+
+--
+-- Index pour la table `region`
+--
+ALTER TABLE `region`
+  ADD PRIMARY KEY (`id_region`),
+  ADD KEY `id_pays` (`id_pays`);
+
+--
+-- Index pour la table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id_type`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `bureau`
+--
+ALTER TABLE `bureau`
+  MODIFY `id_bureau` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `localite`
+--
+ALTER TABLE `localite`
+  MODIFY `id_localite` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `mandat`
+--
+ALTER TABLE `mandat`
+  MODIFY `id_mandat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `membre`
+--
+ALTER TABLE `membre`
+  MODIFY `id_membre` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `pays`
+--
+ALTER TABLE `pays`
+  MODIFY `id_pays` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `poste`
+--
+ALTER TABLE `poste`
+  MODIFY `id_poste` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `region`
+--
+ALTER TABLE `region`
+  MODIFY `id_region` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `bureau`
+--
+ALTER TABLE `bureau`
+  ADD CONSTRAINT `bureau_ibfk_1` FOREIGN KEY (`id_localite`) REFERENCES `localite` (`id_localite`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bureau_ibfk_2` FOREIGN KEY (`id_type`) REFERENCES `type` (`id_type`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bureau_ibfk_3` FOREIGN KEY (`id_mandat`) REFERENCES `mandat` (`id_mandat`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `localite`
+--
+ALTER TABLE `localite`
+  ADD CONSTRAINT `localite_ibfk_1` FOREIGN KEY (`id_region`) REFERENCES `region` (`id_region`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD CONSTRAINT `membre_ibfk_1` FOREIGN KEY (`id_bureau`) REFERENCES `bureau` (`id_bureau`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `membre_ibfk_2` FOREIGN KEY (`id_poste`) REFERENCES `poste` (`id_poste`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `region`
+--
+ALTER TABLE `region`
+  ADD CONSTRAINT `region_ibfk_1` FOREIGN KEY (`id_pays`) REFERENCES `pays` (`id_pays`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
